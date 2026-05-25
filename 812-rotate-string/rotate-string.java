5d086ce555af929042beb8c0837b5a1ecfb80e95
Aditya Pratap <adityapratap8957@gmail.com>
2026-05-25 13:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (98.04%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,15 +3,7 @@ class Solution {
        
         if(s.length() != goal.length()) return false;
         String temp = s+s;
-        for(int i=0; i<s.length(); i++){
-            int j=0;
-            int k=i;
-            while(k<temp.length() && j<goal.length() && temp.charAt(k) == goal.charAt(j)){
-                k++;
-                j++;
-            }
-            if(j == goal.length()) return true;
-        }
-        return false;
+        
+        return temp.contains(goal);
     }
 }
